381cc0dc1c7896668b8dea5e286477279a8e33d6
horler408 <horler.web@gmail.com>
2020-10-03 04:08:20 +0100

    Adding slider

--- js/app.js
+++ js/app.js
@@ -75,3 +75,43 @@ scrolllinks.forEach((link) => {
     linksContainer.style.height = 0;
   });
 });
+
+//Carousel
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click", () => {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  counter--;
+  carousel();
+});
+
+var carousel = () => {
+  if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.style.display = "none";
+  }
+
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else {
+    prevBtn.style.display = "none";
+  }
+
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
+
+prevBtn.style.display = "none";
